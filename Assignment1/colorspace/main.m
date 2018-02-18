c94374115361b94a3d1933f23b0f032b28e32b59
Snikkie <nicole.ferreirasilverio@student.uva.nl>
2018-02-18 15:02:06 +0100

    2nd part of the exercise minus grayscale

--- Assignment1/colorspace/main.m
+++ Assignment1/colorspace/main.m
@@ -6,13 +6,13 @@ close all
 
 I = imread('peppers.png');
 
-J = ConvertColorSpace(I,'opponent');
+%J = ConvertColorSpace(I,'opponent');
  
 % close all
-% J = ConvertColorSpace(I,'rgb');
+%J = ConvertColorSpace(I,'rgb');
 
 % close all
-% J = ConvertColorSpace(I,'hsv');
+%J = ConvertColorSpace(I,'hsv');
 
 % close all
 % J = ConvertColorSpace(I,'ycbcr');
