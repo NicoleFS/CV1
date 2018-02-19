1a49553dfd7d3fad482b5a473935fa054ede57da
Snikkie <nicole.ferreirasilverio@student.uva.nl>
2018-02-19 15:45:12 +0100

    Colorspace grayscale + modified visualize so that grayscale also can be shownin 1 image

--- Assignment1/colorspace/visualize.m
+++ Assignment1/colorspace/visualize.m
@@ -1,12 +1,18 @@
 function visualize(input_image)
 
 image_values = input_image;
+[~, ~, size3] = size(input_image);
 
-subplot(2,2,1), imshow(input_image)
-size_img_matrix = size(image_values);
-
-for i = 1:size_img_matrix(3)
-    subplot(2,2,i+1), imshow(image_values(:,:,i))
+if size3 == 3
+    subplot(2,2,1), imshow(input_image)
+    size_img_matrix = size(image_values);
+    for i = 1:size_img_matrix(3)
+        subplot(2,2,i+1), imshow(image_values(:,:,i))
+    end
+elseif size3 == 4
+    for i = 1:size3
+        subplot(2,2,i), imshow(image_values(:,:,i))
+    end
 end
 
 end
