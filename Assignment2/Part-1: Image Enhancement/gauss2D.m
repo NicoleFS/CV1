979f1e59e62da0a107a96e519f28a60d2d43cdae
JCZuurmond <jczuurmond@gmail.com>
2018-02-22 14:05:24 +0100

    2d gauss

--- Assignment2/Part-1: Image Enhancement/gauss2D.m
+++ Assignment2/Part-1: Image Enhancement/gauss2D.m
@@ -1,3 +1,8 @@
-function G = gauss2D( sigma , kernel_size )
+function G = gauss2D(sigma, kernel_size)
     %% solution
+    G = zeros(kernel_size, kernel_size);
+    Gx = gauss1D(sigma, kernel_size);
+    for i = 1:kernel_size
+        G(i, :) = Gx(i) * Gx;
+    end
 end
