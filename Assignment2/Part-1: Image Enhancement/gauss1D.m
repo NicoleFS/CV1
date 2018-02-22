6cfadb6eed9e203dd57f39e599e69f7d3897dd1c
JCZuurmond <jczuurmond@gmail.com>
2018-02-22 14:05:11 +0100

    1d gauss

--- Assignment2/Part-1: Image Enhancement/gauss1D.m
+++ Assignment2/Part-1: Image Enhancement/gauss1D.m
@@ -1,7 +1,11 @@
-function G = gauss1D( sigma , kernel_size )
-    G = zeros(1, kernel_size);
+function G = gauss1D(sigma, kernel_size)
+    G = ones(1, kernel_size)./(sigma*sqrt(2*pi));
     if mod(kernel_size, 2) == 0
         error('kernel_size must be odd, otherwise the filter will not have a center to convolve on')
     end
     %% solution
+    r = floor(kernel_size/2);
+    x = -r:r;
+    G = G .* exp(-((x.*x)/(2*sigma^2)));
+    G = G ./ sum(G);
 end
