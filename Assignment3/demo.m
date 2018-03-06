65e033fab3d9bb82b92dcc8892a994f4d2456f98
JCZuurmond <jczuurmond@gmail.com>
2018-03-06 16:02:33 +0100

    harris corner detector

--- Assignment3/demo.m
+++ Assignment3/demo.m
@@ -1,11 +1,12 @@
-im = imread('pingpong/0000.jpeg');
+im = rgb2gray(im2double(imread('pingpong/0000.jpeg')));
+im = rgb2gray(im2double(imread('person_toy/00000001.jpg')));
 % TODO: not just use first channel of the image
 
-threshold = 243;
+threshold = 0.09;
 n = 3;
-k = 11;
-sigma = 1;
-[H, r, c] = harris_corner_detector(im(:, :, 1), threshold, n, k, sigma);
+k = 5;
+sigma = .5;
+[H, r, c] = harris_corner_detector(im, threshold, n, k, sigma);
 
 for angle=0:15:100
    im_rot = imrotate(im, angle); 
