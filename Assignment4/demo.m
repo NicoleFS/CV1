987ffe26e6f83d4c6e62e9fe18de45a3a937e1f5
JCZuurmond <jczuurmond@gmail.com>
2018-03-13 12:53:54 +0100

    demo script for running all functions

--- Assignment4/demo.m
+++ Assignment4/demo.m
@@ -1,3 +1,13 @@
+clc
+clear all
+close all
+
+% Load VLfeat
+
+vlfeat_root_dir = 'vlfeat-0.9.21';
+run(strcat(vlfeat_root_dir, '/toolbox/vl_setup'));
+vl_version verbose
+
 %% The boats
 im1 = imread('boat1.pgm');
 im2 = imread('boat2.pgm');
@@ -12,3 +22,5 @@ imshow(im2double(im2));
 title('boat2');
 
 %%
+
+[f1, f2] = keypoint_matching(im1, im2);
