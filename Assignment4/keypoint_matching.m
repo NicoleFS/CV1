95328b02e4ce5e0b6ef962d5799d154c0b266af0
JCZuurmond <jczuurmond@gmail.com>
2018-03-13 12:54:06 +0100

    keypoint matching function of two images

--- Assignment4/keypoint_matching.m
+++ Assignment4/keypoint_matching.m
@@ -1,4 +1,4 @@
-function [r, c] = keypoint_matching(im1, im2)
+function [f1, f2] = keypoint_matching(im1, im2)
 % keypoint_maching Finds matching keypoint on the image pairs.
 %
 %[r, c] = keypoint_matching(im1, im2)
@@ -6,12 +6,32 @@ function [r, c] = keypoint_matching(im1, im2)
 %   im2             Second grey scale image
 %
 % Output
-%   [r, c]          Rows and columns of the keypoints.
+%   [f1, f2]        Features of im1 that match with features of im2.
+
+r = zeros(size(im1));
+c = zeros(size(im2));
 
 % Conver to single precision
-im1 = single(im1);
-im2 = single(im2);
+I1 = single(im1);
+I2 = single(im2);
 
-[f,d] = vl_sift(I);
+% Show 50 randomly chosen sift features
+% [f, d] = vl_sift(I1);
+% perm = randperm(size(f, 2)) ;
+% sel = perm(1:50) ;
+% 
+% figure(1);
+% imshow(im1);
+% hold on
+% h = vl_plotframe(f(:,sel)) ;
+% set(h,'color','y','linewidth',2);
+% hold off
+
+[f1, d1] = vl_sift(I1);
+[f2, d2] = vl_sift(I2);
+[matches, ~] = vl_ubcmatch(d1, d2);
+
+f1 = f1(:, matches(:, 1));
+f2 = f2(:, matches(:, 2));
 
 end
