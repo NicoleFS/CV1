cfa3026da439772b112c4bd019b116250d2b7d9e
Snikkie <nicole.ferreirasilverio@student.uva.nl>
2018-03-24 00:27:15 +0100

    k-means and push new images unto cluster centres done

--- FinalProject/part1/extract_features.m
+++ FinalProject/part1/extract_features.m
@@ -13,61 +13,39 @@ if color_scheme == "gray"
     end
     
     if type_of_sift == "dense"
-        [~, descriptors] = vl_dsift(image, 10);
+        [~, descriptors] = vl_dsift(image, 'step', 10);
     elseif type_of_sift == "normal"
         [~, descriptors] = vl_sift(image);
     end
 
-elseif color_scheme == "RGB"
+else
     if sizez == 3
+        
         temp_im = single(rgb2gray(input_im));
-        input_im = single(input_im);
-        if type_of_sift == "dense"
-            [~, descriptors] = vl_phow(input_im, 'step', 10, 'color','rgb');            
-        elseif type_of_sift == "normal"
-            [temp_frames, ~] = vl_sift(temp_im);
-            [~, temp_descriptors1] = vl_sift(input_im(:,:,1), 'frames', temp_frames);
-            [~, temp_descriptors2] = vl_sift(input_im(:,:,2), 'frames', temp_frames);
-            [~, temp_descriptors3] = vl_sift(input_im(:,:,3), 'frames', temp_frames);
-            descriptors = cat(1, temp_descriptors1, temp_descriptors2, temp_descriptors3);
+        
+        if color_scheme == "RGB"            
+            input_im = single(input_im);
+        elseif color_scheme == "rgb"
+            input_im = single(rgb2normedrgb(input_im));
+        elseif color_scheme == "opponent"
+            input_im = single(rgb2opponent(input_im));
         end
         
-    end    
-        
-    
-elseif color_scheme == "rgb"
-    if sizez == 3
-        temp_im = single(rgb2gray(input_im));
-        input_im = single(rgb2normedrgb(input_im));
         if type_of_sift == "dense"
-            [~, descriptors] = vl_phow(input_im, 'step', 10, 'color', 'rgb');
-        elseif type_of_sift == "normal"
-            [temp_frames, ~] = vl_sift(temp_im);
-            [~, temp_descriptors1] = vl_sift(input_im(:,:,1), 'frames', temp_frames);
-            [~, temp_descriptors2] = vl_sift(input_im(:,:,2), 'frames', temp_frames);
-            [~, temp_descriptors3] = vl_sift(input_im(:,:,3), 'frames', temp_frames);
-            descriptors = cat(1, temp_descriptors1, temp_descriptors2, temp_descriptors3);
-        end
-        
-    end
-    
-elseif color_scheme == "opponent"
-    if sizez == 3
-        temp_im = single(rgb2gray(input_im));
-        input_im = single(rgb2opponent(input_im));
-        if type_of_sift == "dense"
-            [~, descriptors] = vl_phow(input_im, 'step', 10, 'color', 'opponent');
-        elseif type_of_sift == "normal"
-            [temp_frames, ~] = vl_sift(temp_im);
-            [~, temp_descriptors1] = vl_sift(input_im(:,:,1), 'frames', temp_frames);
-            [~, temp_descriptors2] = vl_sift(input_im(:,:,2), 'frames', temp_frames);
-            [~, temp_descriptors3] = vl_sift(input_im(:,:,3), 'frames', temp_frames);
-            descriptors = cat(1, temp_descriptors1, temp_descriptors2, temp_descriptors3);
-        end
-        
-    end
             
-    
+            [~, descriptors] = vl_phow(input_im, 'step', 10, 'color','rgb'); 
+            
+        elseif type_of_sift == "normal"
+            
+            [temp_frames, ~] = vl_sift(temp_im);
+            [~, temp_descriptors1] = vl_sift(input_im(:,:,1), 'frames', temp_frames);
+            [~, temp_descriptors2] = vl_sift(input_im(:,:,2), 'frames', temp_frames);
+            [~, temp_descriptors3] = vl_sift(input_im(:,:,3), 'frames', temp_frames);
+            descriptors = cat(1, temp_descriptors1, temp_descriptors2, temp_descriptors3);
+        end
+        
+    end 
 end
 
 end
+    
